a36d1b2c1f31eb2f28a0680cf8a51418b3f4fd11
Yannyezixin <646840080@qq.com>
2015-05-06 16:10:47 +0800

    add controller and helper

--- gdut/app/controllers/search_controller.rb
+++ gdut/app/controllers/search_controller.rb
@@ -3,10 +3,32 @@ class SearchController < ApplicationController
   layout "search"
 
   def index
+    Rails.logger.debug(Rails.application.config.avatar_path)
   end
 
   def search
-    render json: {"message" => "查不到相关信息", "status" => "error"}
+    sno = params[:sno]
+    if not ApplicationHelper.is_vaild_sno sno
+      render json: {"message" => "学号格式不正确", "status" => "error"} and return
+    end
+
+    if ApplicationHelper.has_avatar sno
+      render json: {"imgUrl" => view_context.image_url("/assets/avatar/#{sno}.jpg"), "status" => "success"} and return
+    end
+
+    scrapy = GdutScrapy.new
+    status = scrapy.scrapy({
+      :sno => sno,
+      :storage_path => Rails.application.config.abs_avatar_path,
+      :username => Rails.application.config.gd_username,
+      :password => Rails.application.config.gd_password
+    })
+
+    if not status
+      render json: {"message" => "查不到相关信息", "status" => "error"} and return 
+    else
+      render json: {"imgUrl" => view_context.image_url("/assets/avatar/#{sno}.jpg"), "status" => "success"} and return
+    end
   end
 end
 
--- gdut/app/helpers/application_helper.rb
+++ gdut/app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def self.is_vaild_sno sno
+
+    return false unless sno.length == 10
+    sex = sno[0..1]
+    return false unless (sex == '31' or sex == '32')
+    year = sno[2..3].to_i
+    return false unless year.between?(10, 15)
+    order_pre = sno[4..5]
+    return false unless (order_pre == '00' or order_pre == '01')
+    order = sno[6..-1].to_i
+    return false unless order.between?(0, 10000)
+
+    return true
+  end
+  
+  def self.has_avatar sno
+    path = Rails.application.config.abs_avatar_path + "/#{sno}.jpg"
+    return File.exist?(path)
+  end
 end
